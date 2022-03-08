bab1ab35122e050371ceb017dd60d4ef4993cd94
Joe De Miro <42090446+JoDeMiro@users.noreply.github.com>
2022-03-08 20:51:23 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -473,9 +473,10 @@ app = Flask(__name__)
 def index():
     return 'Web App with Python Flask!'
 
-# app.run(host='0.0.0.0', port=81)
+from waitress import serve
+app.run(host='0.0.0.0', port=81)
 # app.run(host='127.0.0.1', port=81)
-app.run(host='192.168.0.247', port=81)
+# app.run(host='192.168.0.247', port=81)
 
 
 
