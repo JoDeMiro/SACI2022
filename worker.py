b3d5c3db6af4adf41fc37d32db72d651e6789ffb
Joe De Miro <42090446+JoDeMiro@users.noreply.github.com>
2022-03-08 20:09:42 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -4,6 +4,31 @@ print('---------------------------------------------------------------')
 print('                         HELLO WORKER                          ')
 print('---------------------------------------------------------------')
 
-import os
 
-print(os.listdir('./Input'))
+print('---------------------------------------------------------------')
+print('                         IMPORT                                ')
+print('---------------------------------------------------------------')
+
+
+import os
+import sys
+import pprint
+import sklearn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.neural_network import MLPRegressor
+
+print('Python version:{}'.format(sys.version))
+print('Numpy version:{}'.format(np.__version__))
+print('Pandas version:{}'.format(pd.__version__))
+print('Sci-Kit Learn version:{}'.format(sklearn.__version__))
+
+
+print('---------------------------------------------------------------')
+print('                         IMPORT                                ')
+print('---------------------------------------------------------------')
