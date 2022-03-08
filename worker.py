24f192a61482d1c9b27f9497ea0d66f0b119cd9a
Joe De Miro <42090446+JoDeMiro@users.noreply.github.com>
2022-03-08 20:49:05 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -474,7 +474,11 @@ def index():
     return 'Web App with Python Flask!'
 
 # app.run(host='0.0.0.0', port=81)
-app.run(host='127.0.0.1', port=81)
+# app.run(host='127.0.0.1', port=81)
+app.run(host='192.168.0.247', port=81)
+
+
+
 
 
 
