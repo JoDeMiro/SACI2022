25a2d8089028781131284621a3d60c70d38b4608
Joe De Miro <42090446+JoDeMiro@users.noreply.github.com>
2022-03-08 22:09:13 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -462,12 +462,19 @@ print('---------------------------------------------------------------')
 print('                       FLASK                                   ')
 print('---------------------------------------------------------------')
 
+# https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/
 
 
 
 from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/models'
+ALLOWED_EXTENSIONS = {'joblib', 'txt'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -475,13 +482,54 @@ def index():
 
 
 
-from waitress import serve
-# serve(app, host="0.0.0.0", port=81)
-# serve(app, host="127.0.0.1", port=81)
-# serve(app, host="192.168.0.247", port=81)
 
-# app.run(host='127.0.0.1', port=81)
-# app.run(host='192.168.0.247', port=81)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/model', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+
+
+
+
+
+
+
+from waitress import serve
+# serve(app, host="0.0.0.0", port=8080)
+# serve(app, host="127.0.0.1", port=8080)
+# serve(app, host="192.168.0.247", port=8080)
+
+# app.run(host='127.0.0.1', port=8080, DEBUG=True)
+# app.run(host='192.168.0.247', port=8080, DEBUG=True)
 
 def create_app():
    return app
