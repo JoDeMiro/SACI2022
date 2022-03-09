84882aa5f90a759da65f1467a2329c438f32905c
Joe De Miro <42090446+JoDeMiro@users.noreply.github.com>
2022-03-09 16:16:00 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -648,7 +648,7 @@ def evaluate_model(mlp):
     
     # ki kéne venni a resultból, csak a 'gain' értéket
     print('----------------aaaaaaaaaaaaa-----------------')
-    print(type(result)
+    print(type(result))
     print('-----------------oooooooooo------------------')
     
     # küldjük el az eredményt a Drivernek
