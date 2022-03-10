cf0b8184a8ff17feef5f81316f0d36f033ae23d8
JoDeMiro <joedemiro@pintye.com>
2022-03-10 08:23:49 +0100

    worker setup with parameter

--- worker.py
+++ worker.py
@@ -707,7 +707,7 @@ def testpoint():
 
 
 @app.route('/setup', methods=['GET'])
-def initialize_params(_parameters=parameters, _nRowsRead=3000, _window=20, _threshold = -1000.0):
+def initialize_params(_parameters=parameters, _nRowsRead=2999, _window=20, _threshold = -1000.0):
     '''
     Bemenete az a Parameters objektum amit a program elején létrehoztunk,
     illetve azok az értékek amelyekre be akarjuk ezt állítani
