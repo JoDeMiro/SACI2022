f597e12f64bbd848f94be99bad51a080b8a9c9d2
JoDeMiro <joedemiro@pintye.com>
2022-03-11 17:58:09 +0100

    new worker_id parameter

--- driver.py
+++ driver.py
@@ -444,29 +444,6 @@ def empty_func():
 	print('a')
 	return 'abc'
 
-# evolution DEV
-@app.route('/evolution')
-def evolution_dev():
-	# resp = initialize_workers()
-	new_clf = deepcopy(clf)
-	old_coefs_ = deepcopy(new_clf.coefs_)
-	print(old_coefs_)
-	for i in range(3):
-		print('fuck')
-	new_coefs_ = randomer.randomize(coefs = old_coefs_, factor = 1000)
-	print(new_coefs_)
-	new_clf.coefs_ = new_coefs_        # el kéne küldeni a workereknek az új modelt.
-	new_clf_file_name = 'model3.joblib'           # ezt is váltogatni kell kérdés, hogy a tuloldalon milyen néven menti el?
-	joblib.dump(new_clf, new_clf_file_name)         # el kéne küldeni egy adott workingernek (speckó nevet kell adni neki)
-	worker_address = 'http://192.168.0.247:8080' # ezt majd mindíg váltogatni kell
-	call_worker_sender(worker_address, new_clf_file_name)
-	new_clf = 10
-	abc = empty_func()
-	print('______végig mentünk az össezs worker initializejan______')
-	print('--------------------------------------------------------')
-	my_result = 'mumbapa'
-	return my_result
-
 
 # evolution DEV
 @app.route('/evolution2')
@@ -476,6 +453,9 @@ def evolution_dev2():
 	old_coefs_ = deepcopy(new_clf.coefs_)
 	print('--------------- OLD COEFS -------------')
 	print(old_coefs_)
+	print('ááááááááááááááááááááá')
+	print(workers_addresses)
+	print(len(workers_addresses))
 	for i in range(3):
 		print('fuck')
 		new_coefs_ = randomer.randomize(coefs = old_coefs_, factor = 1000)
